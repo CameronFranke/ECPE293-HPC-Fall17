6880a39498fadde45fea7a164e3d31c15e44cf03
Cameron <C_franke1@u.pacific.edu>
2017-02-02 10:51:14 -0800

    fixed mistake, added README

--- Program1/ghosts_cfranke.c
+++ Program1/ghosts_cfranke.c
@@ -1,16 +1,10 @@
 #include "image.h"
 #include <stdio.h>
-#include "time.h"
+#include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
 
 void write_segment(int row_start, int row_stop, int image_size, int *host_image, int x_dimension, int y_dimension, int pid, int processors); 
-
-typedef struct 
-{
-	long tv_sec;
-	long tv_usec;
-} timeval;
 
 int main(int argc, char *argv[])
 {
@@ -24,14 +18,14 @@ int main(int argc, char *argv[])
 	}
 	
 	//	// start timer //	// 
-	struct timeval start;  
+	struct timeval start, end;  
 	gettimeofday(&start, NULL);
 
 
 	printf("Processors: %d\n", p);
 	printf("Filename: %s\n", filename);
-	char *tempname;
-        strcpy(tempname, filename);	
+	char tempname[30];
+    strcpy(tempname, filename);	
 
 	// parse out image size 
 	char *parse; 
@@ -54,12 +48,7 @@ int main(int argc, char *argv[])
 	read_image_template(filename, &image, &image_size, &image_size); 
 		
 
-	// DEBUGGING: This will normally be in a loop and operate on all segments
-	// 	      Starting with just the first segment 
-
-	//write_segment(0, 127, 256, image, segment_x, segment_y);
 	// call write segment for each segemnt of the image 
-
 	int segment_start, segment_stop; 
 	for(int x = 0; x < p; x++)
 	{
@@ -70,9 +59,9 @@ int main(int argc, char *argv[])
 
 
 
-	//	// stop timer //	//
-	//gettimeofday(&end, NULL);
-	//printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+		// stop timer //	//
+	gettimeofday(&end, NULL);
+	printf("Execution time(microseconds): %ld\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
 
 	return 0; 
 }
@@ -91,6 +80,46 @@ void write_segment(int row_start, int row_stop, int image_size, int *host_image,
 			image_segment[((j + 1)*size_of_row ) + (i + 1)] = host_image[(row_start*image_size) + (j*image_size) + i];
 		}
 	}
+
+	// fill in top/ bottom 
+	// if not first or last segment 
+	if (pid == 0)
+	{
+		// copy top row
+		for(int i=0; i<x_dimension; i++)
+    	{
+        	image_segment[i] = image_segment[size_of_row + i];
+		}
+	}
+	else
+	{
+		// get pixels for top row from host image
+		for(int i=0; i<image_size; i++)
+        {
+			image_segment[i + 1] = host_image[((row_start - 1) *image_size) + i];
+        }
+ 
+	}
+
+	if (pid == processors - 1) // create ghost bottom row
+	{
+		for(int i=0; i<x_dimension; i++)
+    	{
+        	image_segment[(y_dimension-1) * size_of_row + i] = image_segment[(y_dimension-2) * size_of_row + i];
+    	} 
+
+	}
+	else
+	{
+		// get bottom row of pixels from host image 
+		for(int i=0; i<x_dimension; i++)
+        {
+            image_segment[(y_dimension-1) * size_of_row + 1] = host_image[((row_stop + 1) *image_size) + i];
+        }
+
+	}	
+
+
 	// fill in values for left and right ghost columns 
 	for (int j=0; j<y_dimension; j++)
 	{
@@ -98,16 +127,6 @@ void write_segment(int row_start, int row_stop, int image_size, int *host_image,
 		image_segment[j*size_of_row + x_dimension - 1] = image_segment[j*size_of_row + x_dimension - 2]; //fill in right side 
 	}
 
-	
-	// fill in values for top and bottom ghost rows 
-	for(int i=0; i<x_dimension; i++)
-	{
-		// top side 
-		image_segment[i] = image_segment[size_of_row + i];	
-
-		// bottom side 
-		image_segment[(y_dimension-1) * size_of_row + i] = image_segment[(y_dimension-2) * size_of_row + i];
-	}
 	
 	// assemble filename string 	
 	char my_segment_filename[20] = "op_";
