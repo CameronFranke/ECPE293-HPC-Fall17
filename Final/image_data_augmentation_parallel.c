b0b5a739650a21820b9ccce11e327e46d96520e5
cameron <C_franke1@u.pacific.edu>
2017-04-30 17:07:49 -0700

    added more images

--- Final/image_data_augmentation_parallel.c
+++ Final/image_data_augmentation_parallel.c
@@ -14,6 +14,7 @@ int main()
     int omp_procs = omp_get_num_procs();
     printf("Processors: %d\n", omp_procs);
     omp_set_num_threads(omp_procs);
+    //omp_set_num_threads(1);
 
     std::ifstream inLabels; 
     std::string data, filenames[1000], labelstr, filename;
@@ -49,7 +50,7 @@ int main()
 void augmentFrames(std::string filename, float label, std::ofstream &outfile)
 {
     int dst_width=320, dst_height=180, warp_factor=20;
-    float mirrored_label, lw1, rw1;
+    float mirrored_label, lw1, rw1, lw2, rw2;
     std::string filedir; 
     
     if (outfile == NULL)
@@ -87,7 +88,6 @@ void augmentFrames(std::string filename, float label, std::ofstream &outfile)
     resize(warped1, warped1, mySize);
     imwrite("Results/Frame_" + filename  + "_0.jpg", warped1);
 
-
     pts2[0] = Point2f(0, 0);
     pts2[1] = Point2f(320, warp_factor);
     pts2[2] = Point2f(0, dst_height);
@@ -102,14 +102,49 @@ void augmentFrames(std::string filename, float label, std::ofstream &outfile)
     lw1 = label + (warp_factor*.01);
     rw1 = label - (warp_factor*.01);
 
+    /////////// Change warp factor and compute 2 new frames ///////////
+    warp_factor = 40;
+    Rect crop2(0, warp_factor, dst_width, dst_height-warp_factor*2);
+    
+    pts2[0] = Point2f(0, warp_factor);
+    pts2[1] = Point2f(320, 0);
+    pts2[2] = Point2f(0, dst_height-warp_factor);
+    pts2[3] = Point2f(320, dst_height);
+
+    Transform = getPerspectiveTransform(pts1, pts2);
+    warpPerspective(img, warped1, Transform, mySize);
+    warped1 = warped1(crop2);
+    resize(warped1, warped1, mySize);
+    imwrite("Results/Frame_" + filename  + "_3.jpg", warped1);
+
+    pts2[0] = Point2f(0, 0);
+    pts2[1] = Point2f(320, warp_factor);
+    pts2[2] = Point2f(0, dst_height);
+    pts2[3] = Point2f(320, dst_height-warp_factor);
+
+    Transform = getPerspectiveTransform(pts1, pts2);
+    warpPerspective(img, warped2, Transform, mySize);
+    warped2 = warped2(crop2);
+    resize(warped2, warped2, mySize);
+    imwrite("Results/Frame_" + filename + "_4.jpg", warped2);
+
+    lw2 = label + (warp_factor*.0185);
+    rw2 = label - (warp_factor*.0185);
+
+    ////////////// write to file ///////////////////////////////////	    
+
     std::cout << "label: " << label << "\n";
-    std::cout << "left warp label: " << lw1 << "\n";
-    std::cout << "right warp label: " << rw1 << "\n";
+    std::cout << "left 1 warp label: " << lw1 << "\n";
+    std::cout << "right 1 warp label: " << rw1 << "\n";
+    std::cout << "left 2 warp label: " << lw2 << "\n";
+    std::cout << "right 2 warp label: " << rw2 << "\n";
 
     // write label to file
     outfile << filename + "_0:" + std::to_string(lw1) + "\n";
     outfile << filename + "_1:" + std::to_string(rw1) + "\n";
     outfile << filename + "_2:" + std::to_string(label*-1) + "\n";
+    outfile << filename + "_3:" + std::to_string(lw2) + "\n";
+    outfile << filename + "_4:" + std::to_string(rw2) + "\n";
 }
 
 
